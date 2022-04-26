6c1e76015c32376c1a0239adc72470c3b9506f6d
komeil Parseh <ahmdparsh129@gmail.com>
2022-04-27 01:26:01 +0430

    💫 add new function
    * string_to_bytes: for convert str(s) to byte(s)
    * string_to_hash: Returns the sha3_512 hash of a string.
    * check_password_hash: Checks if a password matches a hash.

--- breeze/utils.py
+++ breeze/utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from hashlib import sha3_512
 
 
 def get_current_time():
@@ -6,3 +7,24 @@ def get_current_time():
     Returns the current time in UTC.
     """
     return datetime.utcnow()
+
+
+def string_to_bytes(string):
+    """
+    Converts a string to bytes.
+    """
+    return bytes(string, "utf-8")
+
+
+def string_to_hash(string):
+    """
+    Returns the sha3_512 hash of a string.
+    """
+    return sha3_512(string_to_bytes(string)).hexdigest()
+
+
+def check_password_hash(password, hash):
+    """
+    Checks if a password matches a hash.
+    """
+    return string_to_hash(password) == hash
