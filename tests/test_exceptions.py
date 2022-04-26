87023f199bfc6922077cbe99326d2187c6d9d21b
komeil Parseh <ahmdparsh129@gmail.com>
2022-04-27 01:29:21 +0430

    🧪 add tests for breeze.exc

---
+++ tests/test_exceptions.py
@@ -0,0 +1,20 @@
+import pytest
+
+from breeze import exc
+
+
+def test_breeze_exception():
+    with pytest.raises(exc.BreezeException):
+        raise exc.BreezeException("Test")
+
+
+def test_permission_error_exception():
+    """Test this duplication and do not need this, but we test for coverage"""
+    with pytest.raises(exc.PermissionError):
+        raise exc.PermissionError("Test")
+
+
+def test_empty_error_exception():
+    """Test this duplication and do not need this, but we test for coverage"""
+    with pytest.raises(exc.EmptyError):
+        raise exc.EmptyError("Test")
