9138ac5ebe683f0a1542ed20f226831ce2438850
komeil Parseh <ahmdparsh129@gmail.com>
2022-04-27 19:24:00 +0430

    ✨ breeze can support python 3.7 and upper

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 
 from setuptools import setup  # type: ignore
 
-if sys.version_info < (3, 8, 0):
-    sys.exit("Python 3.8 or later is required. ")
+if sys.version_info < (3, 7, 0):
+    sys.exit("Python 3.7 or later is required. ")
 
 requirements = ["flask", "python-dotenv", "Flask-SQLAlchemy"]
 docs_requirements = [
