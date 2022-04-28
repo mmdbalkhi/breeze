4055173fdc1a861f1fe65e53d51ad2339f70d38d
komeil Parseh <ahmdparsh129@gmail.com>
2022-04-28 12:36:01 +0430

    ✨ Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import sys
 
 from setuptools import setup  # type: ignore
@@ -7,7 +6,12 @@ from setuptools import setup  # type: ignore
 if sys.version_info < (3, 7, 0):
     sys.exit("Python 3.7 or later is required. ")
 
-requirements = ["flask", "python-dotenv", "Flask-SQLAlchemy"]
+requirements = [
+    "flask>=2.1.0",
+    "python-dotenv>=0.20.0",
+    "SQLAlchemy>=0.8.0",
+    "Flask-SQLAlchemy>=2.5.1",
+]
 docs_requirements = [
     "furo",
     "myst-parser",
@@ -21,4 +25,6 @@ dev_requirements = ["pytest", *requirements, *docs_requirements]
 setup(
     install_requires=requirements,
     extras_require={"dev": dev_requirements, "docs": docs_requirements},
+    include_package_data=True,
+    zip_safe=False,
 )
