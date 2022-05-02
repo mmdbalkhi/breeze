8eadf8ec422ac59545a790e80fc31d064ce73aaa
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-02 23:38:27 +0430

    💫 Add bp.auth for auth users

--- breeze/blueprints/auth.py
+++ breeze/blueprints/auth.py
@@ -1,5 +1,4 @@
-from breeze import Auth
-from breeze import exc
+from breeze.auth import Auth
 from breeze.models import User
 from flask import Blueprint
 from flask import flash
@@ -14,56 +13,80 @@ auth = Auth()
 
 @bp.route("/register", methods=("GET", "POST"))
 def register():
-    """Register a new user to the system
+    """Register a new user to the system.
 
     :returns:
-        :class:`flask.Response`: The response to the request
+        :class:`flask.Response`:
+            - if the request method is GET, render the register template
+            - if the request method is POST, register the user and redirect to the login page
+
     """
     if request.method == "POST":
-        username = request.args.get("username")
-        email = request.args.get("email")
-        password = request.args.get("password")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
 
-        try:
+        if not username or not email or not password:
+            error = "please fill out all fields"
+        elif User.query.filter_by(username=username).first():
+            error = f"User {username} is already registered."
+
+        else:
             user = User(username=username, email=email, password=password)
             auth.register(user)
-        except exc.EmptyError as e:
-            flash(e.message)
-            return redirect(url_for("auth.register"))
+
+            flash("Registered successfully.")
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+        return render_template("auth/register.html"), 400
+
     return render_template("auth/register.html")
 
 
 @bp.route("/login", methods=("GET", "POST"))
 def login():
-    """Login a user to the system
+    """Login a user to the system.
 
     :returns:
-        :class:`flask.Response`: The response to the request
+        :class:`flask.Response`:
+            - if the request method is GET, render the login template
+            - if the request method is POST, login the user and redirect to the index page
     """
     if request.method == "POST":
-        username = request.args.get("username")
-        password = request.args.get("password")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
 
-        try:
+        if not username or not password:
+            error = "Username and password are required."
+        else:
             user = User.query.filter_by(username=username).first()
+            if not user or not user.check_password(password):
+                error = "Incorrect username or password."
 
-            if not user.check_password(password):
-                flash("password is not correct")
-
+        if error is None:
             auth.login(user)
-            return redirect(url_for("index"))
-        except exc.EmptyError as e:
-            flash(e.message)
-            return redirect(url_for("auth.login"))
+            return redirect(url_for("auth.index"))
+
+        flash(error)
+        return render_template("auth/login.html"), 400
+
     return render_template("auth/login.html")
 
 
 @bp.route("/logout")
 def logout():
-    """Logout a user from the system
+    """Logout a user from the system.
 
     :returns:
-        :class:`flask.Response`: The response to the request
+        :class:`flask.Response`: redirect to the login page
     """
+    flash("You have been logged out.")
     auth.logout()
-    return redirect(url_for("index"))
+    return redirect(url_for("auth.index"))
+
+
+@bp.route("/")
+def index():
+    return render_template("index.html")
