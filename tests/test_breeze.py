af1c130b9fd5b0d4b5f54a707ce4ef81b224f185
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-06 15:43:26 +0430

    🧪 Increase the percentage of code coverage, breeze/__init__.py

--- tests/test_breeze.py
+++ tests/test_breeze.py
@@ -1,7 +1,23 @@
+from pathlib import Path
+
 from flask import Flask
 
+from breeze import Config
 from breeze import create_app
 
 
 def test_create_app():
     assert isinstance(create_app(), Flask)
+
+
+def test_create_app_if_db_is_exists():
+    path = Path("breeze/breeze.db")
+    path.touch()
+    assert isinstance(create_app(), Flask)
+
+
+def test_touch_dotenv():
+    path = Path(Path.cwd() / ".env")
+    path.write_text("SECRET_KEY='VERY_VERY_SECRET_KEY'")
+
+    assert Config.dotenv_path == str(path)
