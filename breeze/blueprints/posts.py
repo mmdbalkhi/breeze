51b0d7291c0955a117f52d5be57d6fa331dcdee5
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-26 21:09:08 +0430

    add comments for posts

--- breeze/blueprints/posts.py
+++ breeze/blueprints/posts.py
@@ -1,16 +1,16 @@
+from breeze.auth import Auth
+from breeze.models import Comment
+from breeze.models import Post
+from breeze.utils import get_current_time
+from breeze.utils import get_image_from_gravatar
 from flask import abort
 from flask import Blueprint
 from flask import flash
-from flask import g
 from flask import redirect
 from flask import render_template
 from flask import request
 from flask import session
 from flask import url_for
-
-from breeze.auth import Auth
-from breeze.models import Post
-from breeze.utils import get_current_time
 
 bp = Blueprint("posts", __name__, url_prefix="/p")
 auth = Auth()
@@ -29,8 +29,6 @@ def new():
         flash("You must be logged in to create a post")
         return redirect(url_for("auth.login"))
 
-    with open("/tmp/file", "w") as f:
-        f.write(str(g.user))
     if request.method == "POST":
         user_id = session["user_id"]
 
@@ -44,7 +42,9 @@ def new():
         flash("Post created successfully.")
         return redirect(f"/p/{post.id}")
 
-    return render_template("posts/new.html")
+    return render_template(
+        "posts/new.html",
+    )
 
 
 @bp.route("/<int:id>")
@@ -58,6 +58,41 @@ def show(id: int):
         :class:`flask.Response`: The rendered template
     """
     post = Post.get_post_by_id(id)
+    comments = Comment.get_comments_by_post_id(id)
     if not post:
         abort(404)
-    return render_template("posts/show.html", post=post)
+
+    return render_template(
+        "posts/show.html",
+        gravatar=get_image_from_gravatar,
+        post=post,
+        comments=comments,
+    )
+
+
+@bp.route("/<int:id>/delete")
+def delete(id: int):
+    """Delete a post
+
+    :args:
+        ``id`` (`int`): The id of the post to delete
+
+    :returns:
+        :class:`flask.Response`: The redirect to the home page
+    """
+
+    post = Post.get_post_by_id(id)
+    if not post:
+        abort(404)
+
+    if not auth.is_authenticated:
+        flash("You must be logged in to delete a post")
+        return redirect(url_for("auth.login"))
+
+    if post.user_id != session["user_id"]:
+        flash("You are not authorized to delete this post")
+        return redirect(url_for("posts.show", id=id))
+
+    post.delete()
+    flash("Post deleted successfully.")
+    return redirect(url_for("index.index"))
