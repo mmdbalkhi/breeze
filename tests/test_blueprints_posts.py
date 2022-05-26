5c6255eb5a88323155ce41988b0c28adacf28c31
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-26 21:10:59 +0430

    Increase test coverage in posts bp

--- tests/test_blueprints_posts.py
+++ tests/test_blueprints_posts.py
@@ -108,3 +108,98 @@ def test_show_post():
 def test_show_post_error():
     respone = client.get("/p/404")
     assert respone.status_code == 404
+
+
+def test_delete_post():
+    with app.app_context():
+        # insure that the database is empty
+        db.drop_all()
+        db.create_all()
+
+        # create a user
+        user = User(
+            username="test_new_post",
+            email="test@test.com",
+            password="test_new_post",
+        )
+        user.save()
+        client.post(
+            "/u/login", data={"username": "test_new_post", "password": "test_new_post"}
+        )
+
+        # create a post
+        client.post(
+            "/p/new", data={"content": "test create a new post"}, follow_redirects=True
+        )
+
+    response = client.get("/p/1/delete")
+    assert response.status_code == 302
+    assert response.location == "/"
+
+
+def test_delete_post_post_not_found():
+    response = client.get("/p/404/delete")
+    assert response.status_code == 404
+
+
+def test_delete_post_not_login():
+    with app.app_context():
+        # insure that the database is empty
+        db.drop_all()
+        db.create_all()
+
+        # create a user
+        user = User(
+            username="test_new_post",
+            email="test@test.com",
+            password="test_new_post",
+        )
+        user.save()
+        client.post(
+            "/u/login", data={"username": "test_new_post", "password": "test_new_post"}
+        )
+        client.post(
+            "/p/new", data={"content": "test create a new post"}, follow_redirects=True
+        )
+        client.get("/u/logout")
+
+    response = client.get("/p/1/delete")
+    assert response.status_code == 302
+    assert response.location == "/u/login"
+
+
+def test_delete_post_not_auth():
+    with app.app_context():
+        # insure that the database is empty
+        db.drop_all()
+        db.create_all()
+
+        # create a user
+        user = User(
+            username="test_new_post",
+            email="test@test.com",
+            password="test_new_post",
+        )
+        user.save()
+        client.post(
+            "/u/login", data={"username": "test_new_post", "password": "test_new_post"}
+        )
+        client.post(
+            "/p/new", data={"content": "test create a new post"}, follow_redirects=True
+        )
+        client.get("/u/logout")
+
+        user = User(
+            username="test_new_post2",
+            email="test2@test.com",
+            password="test_new_post",
+        )
+        user.save()
+        client.post(
+            "/u/login", data={"username": "test_new_post2", "password": "test_new_post"}
+        )
+
+    response = client.get("/p/1/delete")
+
+    assert response.status_code == 302
+    assert response.location == "/p/1"
