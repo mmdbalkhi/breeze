ecb6756d55c71dcb875e2fb76a3463dd152b009d
komeil Parseh <ahmdparsh129@gmail.com>
2022-05-26 21:11:25 +0430

    remove config test

--- tests/test_breeze.py
+++ tests/test_breeze.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
-from flask import Flask
-
-from breeze import Config
 from breeze import create_app
+from flask import Flask
 
 
 def test_create_app():
@@ -14,10 +12,3 @@ def test_create_app_if_db_is_exists():
     path = Path("breeze/breeze.db")
     path.touch()
     assert isinstance(create_app(), Flask)
-
-
-def test_touch_dotenv():
-    path = Path(Path.cwd() / ".env")
-    path.write_text("SECRET_KEY='VERY_VERY_SECRET_KEY'")
-
-    assert Config.dotenv_path == str(path)
