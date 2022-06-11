373e3f883c3abfd005453287f48336f0d9419d93
komeil Parseh <ahmdparsh129@gmail.com>
2022-06-11 14:47:09 +0430

    coverage

--- tests/test_oauth2.py
+++ tests/test_oauth2.py
@@ -1,18 +1,15 @@
-import unittest
-
 import pytest
 from breeze import Config
 from breeze import GithubOAuth2 as Github
+
+
+gh = Github()
 
 if not (Config.GITHUB_CLIENT_ID or Config.GITHUB_CLIENT_SECRET):  # pragma: no cover
     pytest.skip("GithubOAuth2 Configs are not set", allow_module_level=True)
 
 
-class GithubOAuth2Test(unittest.TestCase):
-    gh = Github()
-
-    def test_create_authorization_data(self):
-        assert isinstance(self.gh.create_authorization_data(), tuple)
-        assert len(self.gh.create_authorization_data()) == 2
-
-    # other method in GithubOAuth2 can't be tested because it's need to Auth user to get token
+# other method in GithubOAuth2 can't be tested because it's need to Auth user to get token
+def test_create_authorization_data():  # pragma: no cover
+    assert isinstance(gh.create_authorization_data(), tuple)
+    assert len(gh.create_authorization_data()) == 2
